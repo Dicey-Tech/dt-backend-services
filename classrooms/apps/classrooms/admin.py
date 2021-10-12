b9e7585e575dc04bb70ff49f7be6a3f0ae6eda2b
Sofiane Bebert <s.bebert@diceytech.co.uk>
2021-10-12 12:54:32 +0000

    refactor: :recycle: improve admin display for classroom

--- classrooms/apps/classrooms/admin.py
+++ classrooms/apps/classrooms/admin.py
@@ -14,17 +14,39 @@ from classrooms.apps.classrooms.models import (
 
 @admin.register(Classroom)
 class ClassroomAdmin(admin.ModelAdmin):
-    pass
+    """Admin configuration for the Classroom model."""
+
+    list_display = [
+        "uuid",
+        "name",
+        "active",
+        "school",
+    ]
+    list_filter = ["active"]
 
 
 @admin.register(ClassroomEnrollment)
 class ClassroomEnrollmentAdmin(admin.ModelAdmin):
-    pass
+    """Admin configuration for the ClassroomEnrollment model."""
+
+    list_display = [
+        "user_id",
+        "classroom_instance",
+        "staff",
+    ]
+    list_filter = ["staff"]
+    search_fields = ["user_id"]
 
 
 @admin.register(CourseAssignment)
 class CourseAssignmentAdmin(admin.ModelAdmin):
-    pass
+    """Admin configuration for the CourseAssignment model."""
+
+    list_display = [
+        "course_id",
+        "classroom_instance",
+    ]
+    search_fields = ["course_id"]
 
 
 @admin.register(ClassroomFeatureRole)
